8d643f3c5ecaa9ca0dfc7bee691f15859db803c5
UnEmotioneD <blackeagle10@icloud.com>
2025-06-09 08:10:39 +0900

    Feature: logo spacing same as aside spacing to left

--- final/index.js
+++ final/index.js
@@ -1,1 +1,24 @@
-console.log("foobar");
+function updateLogoPosition() {
+  var $aside = $("aside");
+  var $logo = $("#logo");
+
+  if ($aside.length === 0 || $logo.length === 0) return;
+
+  var distanceFromLeft = $aside[0].getBoundingClientRect().left;
+
+  // Always keep at least 25px from the left edge
+  var safeLeft = Math.max(distanceFromLeft, 25);
+
+  $logo.css("left", safeLeft + "px");
+}
+
+// Run on page load
+$(document).ready(function () {
+  updateLogoPosition();
+});
+
+// Run on window resize
+$(window).on("resize", function () {
+  updateLogoPosition();
+});
+var aside = document.querySelector("aside");
